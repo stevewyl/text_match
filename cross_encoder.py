7b9919920dc9394eec8c26bfb5a39b10514a4402
stevewyl <stevewyl@163.com>
2021-02-25 00:12:12 +0800

    feat: sbert & cross-encoder test pass

---
+++ cross_encoder.py
@@ -0,0 +1,54 @@
+"""
+Cross Encoder with CE Loss
+Refer from: https://sbert.net/examples/training/cross-encoder/README.html
+"""
+
+import logging
+import math
+import os
+from datetime import datetime
+
+from sentence_transformers.cross_encoder import CrossEncoder
+from sentence_transformers.cross_encoder.evaluation import CEBinaryClassificationEvaluator
+from sentence_transformers import LoggingHandler
+from sentence_transformers.readers import InputExample
+from torch.utils.data import DataLoader
+
+from utils import load_data, get_train_valid
+
+# 日志
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO,
+                    handlers=[LoggingHandler()])
+logger = logging.getLogger(__name__)
+
+# 模型
+# 模型列表: hfl/chinese-macbert-base, hfl/chinese-roberta-wwm-ext
+model = CrossEncoder("hfl/chinese-roberta-wwm-ext", num_labels=1, max_length=40)
+model_save_path = 'output/cross_encoder_roberta_CELoss-' + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+os.makedirs(model_save_path, exist_ok=True)
+num_epochs = 5
+train_batch_size = 64
+
+# 数据
+texts, labels = load_data("data/gaiic_track3/round1_train.tsv", has_label=True)
+x_train, x_valid, y_train, y_valid = get_train_valid(texts, labels)
+train_samples = []
+for (text_a, text_b), label in zip(x_train, y_train):
+    train_samples.append(InputExample(texts=[text_a, text_b], label=int(label)))
+    train_samples.append(InputExample(texts=[text_b, text_a], label=int(label)))
+train_dataloader = DataLoader(train_samples, shuffle=True, batch_size=train_batch_size)
+
+# 评估器
+dev_samples = [InputExample(texts=x, label=int(y)) for x, y in zip(x_valid, y_valid)]
+evaluator = CEBinaryClassificationEvaluator.from_input_examples(dev_samples, name="dev")
+
+# 训练
+warmup_steps = math.ceil(len(train_dataloader) * num_epochs * 0.1)
+logger.info("Warmup-steps: {}".format(warmup_steps))
+model.fit(train_dataloader=train_dataloader,
+          evaluator=evaluator,
+          epochs=num_epochs,
+          warmup_steps=warmup_steps,
+          output_path=model_save_path)
--- sbert.py
+++ sbert.py
@@ -1,20 +1,60 @@
-from sentence_transformers import SentenceTransformer
-from sentence_transformers import InputExample, losses, evaluation
+"""
+SBERT with Online Constract Loss
+Refer from: https://sbert.net/examples/training/quora_duplicate_questions/README.html
+"""
+
+import logging
+import os
+from datetime import datetime
+
+from sentence_transformers import SentenceTransformer, losses, evaluation
+from sentence_transformers import LoggingHandler
+from sentence_transformers.readers import InputExample
 from torch.utils.data import DataLoader
 
 from utils import load_data, get_train_valid
 
-# 数据
-texts, labels = load_data("data/gaiic_train.tsv")
-x_train, x_valid, y_train, y_valid = get_train_valid(texts, labels)
-train_examples = [InputExample(texts=x, label=y) for x, y in zip(x_train, y_train)]
-train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=32)
+# 日志
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO,
+                    handlers=[LoggingHandler()])
+logger = logging.getLogger(__name__)
 
 # 模型
 model = SentenceTransformer("distiluse-base-multilingual-cased")
-train_loss = losses.CosineSimilarityLoss(model)
+model_save_path = 'output/sbert_OnlineConstrativeLoss-' + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+os.makedirs(model_save_path, exist_ok=True)
+num_epochs = 5
+train_batch_size = 64
+
+# 损失函数
+distance_metric = losses.SiameseDistanceMetric.COSINE_DISTANCE
+margin = 0.5
+train_loss = losses.OnlineContrastiveLoss(
+    model=model, distance_metric=distance_metric, margin=margin)
+
+# 数据
+texts, labels = load_data("data/gaiic_track3/round1_train.tsv", has_label=True)
+x_train, x_valid, y_train, y_valid = get_train_valid(texts, labels)
+train_samples = [InputExample(texts=x, label=int(y)) for x, y in zip(x_train, y_train)]
+train_dataloader = DataLoader(train_samples, shuffle=True, batch_size=train_batch_size)
+
+# 评估器
+dev_sentences1 = []
+dev_sentences2 = []
+dev_labels = []
+for (text_a, text_b), label in zip(x_valid, y_valid):
+    dev_sentences1.append(text_a)
+    dev_sentences2.append(text_b)
+    dev_labels.append(int(label))
+binary_acc_evaluator = evaluation.BinaryClassificationEvaluator(
+    dev_sentences1, dev_sentences2, dev_labels)
 
 # 训练
-model.fit(train_objectives=[(train_dataloader, train_loss)], epochs=1, warmup_steps=100)
-
-losses.
+model.fit(
+    train_objectives=[(train_dataloader, train_loss)],
+    evaluator=binary_acc_evaluator,
+    epochs=num_epochs, warmup_steps=1000,
+    output_path=model_save_path
+)
